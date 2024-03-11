4cf5ea6f0039c0429fd258f4eac210920a0e538e
StepanTeshka <leluhinstepan@gmail.com>
2024-03-11 18:07:12 +0300

    delete partixipates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <TimeRoulette />
         <Wheel />
         <InformationParicipate />
-        <ParicipatesInformation />
+        {/* <ParicipatesInformation /> */}
       </div>
     </Bg>
   );
